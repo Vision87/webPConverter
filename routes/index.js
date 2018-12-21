3d949533bc3a1bb99c0422931ef971b95f47bd42
Alessio Moioli <alessio.moioili@vimncontractor.com>
2018-12-21 12:37:57 +0100

    added webp decoder to images types

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
-var express = require('express')
-var router = express.Router()
-var webp = require('webp-converter')
-var uniqid = require('uniqid')
+const express = require('express')
+const router = express.Router()
+const webp = require('webp-converter')
+const uniqid = require('uniqid')
+const fs = require('fs')
 const outputDir = 'output/'
 const webpExtension = '.webp'
 //multer object creation
-var multer  = require('multer')
-var storage = multer.diskStorage({
+const multer  = require('multer')
+const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'public/uploads/')
     },
@@ -15,40 +16,72 @@ var storage = multer.diskStorage({
   }
 })
  
-var upload = multer({ storage: storage })
+const upload = multer({ storage: storage })
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'WebP', descr: 'Convert your image from/to webP' })
-});
+})
 
-router.post('/convert', upload.single('imageupload'), function(req, res, next) {
-  console.log('res->', res)
-  const outputName = uniqid() + req.file.originalname + webpExtension
-  webp.cwebp(req.file.path, outputDir + outputName, "-q 80", function(status) {
+router.post('/convertFromImage', upload.single('imageupload'), function(req, res, next) {
+  console.log('image type->', req.file.mimetype)
+  const outputName = uniqid() + req.file.originalname + webpExtension  
+  if(req.file.mimetype != 'image/gif') {
+    webp.cwebp(req.file.path, outputDir + outputName, "-mt -q 80", function(status) {
 
-  	//if exicuted successfully status will be '100'
-    //if exicuted unsuccessfully status will be '101'
-    
-    //solution for AJAX call
-    // console.log('STATUS--------->',status);
-    // res.json({
-    //   from: req.file.mimetype,
-    //   to: 'image/webp',
-    //   downloadLink: '/download/' + outputName
-    // });
+      //if exicuted successfully status will be '100'
+      //if exicuted unsuccessfully status will be '101'
+      
+      //solution for AJAX call
+      // console.log('STATUS--------->',status);
+      // res.json({
+      //   from: req.file.mimetype,
+      //   to: 'image/webp',
+      //   downloadLink: '/download/' + outputName
+      // });
 
-    //solution for a barebone approach
-    res.render('download', { title: 'Converting result', description: 'Download your converted image', downloadLink: `/download/${outputName}`})
-  });  
-  // res.json();
-});
+      //solution for a barebone approach
+      res.render('download', { title: 'Converting result', descr: 'Download your converted image', downloadLink: `/download/${outputName}`})
+    })
+  } else {
+    webp.gwebp(req.file.path, outputDir + outputName, "-mt -q 80", function(status) {
+
+      //if exicuted successfully status will be '100'
+      //if exicuted unsuccessfully status will be '101'
+      
+      //solution for AJAX call
+      // console.log('STATUS--------->',status);
+      // res.json({
+      //   from: req.file.mimetype,
+      //   to: 'image/webp',
+      //   downloadLink: '/download/' + outputName
+      // });
+
+      //solution for a barebone approach
+      res.render('download', { title: 'Converting result', descr: 'Download your converted gif', downloadLink: `/download/${outputName}`})
+    })
+  }
+})
+
+router.post('/convertToImage', upload.single('imageupload'), function(req, res, next) {
+  console.log('Request -> ', req)
+  console.log('Type -> ', req.body.imageType)
+  const newImageType = req.body.imageType
+  const outputName = uniqid() + req.file.originalname + newImageType  
+  webp.dwebp(req.file.path, outputDir + outputName, "-mt -o", function(status){
+    res.render('download', { title: 'Converting result', descr: `Download your converted ${newImageType}`, downloadLink: `/download/${outputName}`})
+  })
+  
+})
 
 router.get('/download/:imageID', function(req, res, next) {
   const imageID = req.params.imageID
   console.log('imageID ->', imageID)
-  res.download(outputDir + imageID, imageID)
-  // res.render('index', { title: 'WebP', descr: 'Convert your image from/to webP' });
-});
+  // res.download(outputDir + imageID, imageID)
+  var stream = fs.createReadStream(outputDir + imageID)
+    stream.pipe(res).once("close", function () {
+        stream.destroy()
+        deleteFile(outputDir + imageID)
+    })
+})
 
-module.exports = router;
+module.exports = router
