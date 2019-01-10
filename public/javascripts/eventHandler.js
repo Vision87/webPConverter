3c819d558590297739d619bba8182ee5f7990ebc
Alessio Moioli <alessio.moioili@vimncontractor.com>
2019-01-10 18:11:40 +0100

    added tabs

--- public/javascripts/eventHandler.js
+++ public/javascripts/eventHandler.js
@@ -1,18 +1,23 @@
-const convertForm = new Vue({
-    el: '#convertForm',
-    data:  {
-        name: 'Hello Vue!',
-        message: 'Razza di cane!'
-    },
-    methods: {
-        clickSubmit: (e) => {
-            alert('sto ' + convertForm.name)
-            alert(convertForm.message)
-        },
-        submitForm: (event) => {
-            if (event) event.preventDefault()
+// const convertForm = new Vue({
+//     el: '#convertForm',
+//     data:  {
+//         name: 'Hello Vue!',
+//         message: 'Razza di cane!'
+//     },
+//     methods: {
+//         clickSubmit: (e) => {
+//             alert('sto ' + convertForm.name)
+//             alert(convertForm.message)
+//         },
+//         submitForm: (event) => {
+//             if (event) event.preventDefault()
             
-            alert('You choose the dark side!')
-        }
-    }
-})
+//             alert('You choose the dark side!')
+//         }
+//     }
+// })
+
+new Vue({
+    el: '#tabs',
+    data: { activetab: 1 },
+});
